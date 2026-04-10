fecb597089cf38dc964e50619ff78a9260c65c54
victorharri-chal <victorharrichal@yahoo.com>
2026-04-10 16:32:08 +0200

    fix

--- app/controllers/daily_calorie_requirements_controller.rb
+++ app/controllers/daily_calorie_requirements_controller.rb
@@ -1,9 +1,11 @@
 class DailyCalorieRequirementsController < ApplicationController
   def show
-    @user = current_user
-    @profile = @user.profile
-    @daily_calorie_requirement = @profile.calculate_calories_needed_maintenance
-    @daily_calorie_requirement_weight_loss = @profile.calculate_calories_needed_weight_loss
-    @daily_calorie_requirement_muscle_gain = @profile.calculate_calories_needed_muscle_gain
+    @profile = current_user.profile
+    @calories_maintenance  = @profile.calculate_calories_needed_maintenance
+    @calories_weight_loss  = @profile.calculate_calories_needed_weight_loss
+    @calories_muscle_gain  = @profile.calculate_calories_needed_muscle_gain
+    @protein_goal          = @profile.daily_protein_goal
+    @fats_goal             = @profile.daily_fats_goal
+    @carbs_goal            = @profile.daily_carbs_goal
   end
 end
